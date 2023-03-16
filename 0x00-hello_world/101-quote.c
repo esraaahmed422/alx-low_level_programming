46ce00cc77b003581605737df4ebe0cbb0cf6b63
esraaahmed422 <asraaljmal9@gmail.com>
2023-03-16 15:10:18 -0700

    Intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *followd by a newline, to standard error.
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	printf(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	retutn?(1);
+}
