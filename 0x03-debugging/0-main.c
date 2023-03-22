35feb2f0b9db41f7075a1ba9259eab0ee4f4b24f
esraaahmed422 <asraaljmal9@gmail.com>
2023-03-22 05:05:03 -0700

    0 > 972?

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,21 +4,12 @@
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
-int positive_or_negative(int n){
 
-	if (n > 0)
-		printf("%i is positive\n",n);
-	else if (n == 0)
-		printf("%i is zero\n", n);
-	else
-		printf("%i is negative\n", n);
-}
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 98%2;
+        i= 98 % 2;
         positive_or_negative(i);
-
         return (0);
 }
