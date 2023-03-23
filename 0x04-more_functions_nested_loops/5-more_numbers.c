6fc64cec33c2e84fa23b567e4a6b1e788f33dd53
esraaahmed422 <asraaljmal9@gmail.com>
2023-03-23 12:35:00 -0700

    Numbers constitute the only universal language

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 
 
 /**
- * print_numbers - Print 10 times the numbers since 0 up to 14
+ * more_numbers - Print 10 times the numbers since 0 up to 14
  * Return: 10 times of the numbers since 0 up to 14
  */
 
